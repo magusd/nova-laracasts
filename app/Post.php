bca87929e822cc31ad33f55b37710c5fb965ed3c
magusd <vitor.lobs@gmail.com>
2019-08-22 02:32:15 -0300

    first commit

---
+++ app/Post.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    protected $casts = [
+        'publish_at' => 'datetime',
+        'publish_until' => 'datetime'
+    ];
+}
